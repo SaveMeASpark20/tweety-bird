7430d719f356e9573a08ddafc978a333b26f2bbf
SaveMeASpark20 <legchris12@gmail.com>
2025-03-01 17:11:26 +0800

    try to use another script for render-build sh

--- render-build.sh
+++ render-build.sh
@@ -8,9 +8,13 @@ npm install
 # Compile TypeScript (ensure your tsconfig.json is set up correctly)
 npm run build
 
-# Ensure the Puppeteer cache directory exists
+# Define Puppeteer cache directory
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+BUILD_CACHE_DIR=/opt/render/project/src/.cache/puppeteer/chrome/
+
+# Ensure the Puppeteer cache directory exists
 mkdir -p $PUPPETEER_CACHE_DIR
+mkdir -p $BUILD_CACHE_DIR  # Ensure the target directory exists
 
 # Install Puppeteer and download Chrome if not installed
 if [[ ! -f "$PUPPETEER_CACHE_DIR/chrome-linux/chrome" ]]; then
@@ -22,4 +26,4 @@ fi
 
 # Store/pull Puppeteer cache with build cache
 echo "...Storing Puppeteer Cache in Build Cache"
-cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+cp -R $PUPPETEER_CACHE_DIR $BUILD_CACHE_DIR

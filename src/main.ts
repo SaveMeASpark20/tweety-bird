6624e5d2340ee22851187f0357b19953594f3cdc
SaveMeASpark20 <legchris12@gmail.com>
2025-03-01 23:08:09 +0800

    add the executablepath

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const scrape = async () => {
     console.log("Launching Puppeteer...");
 
     const browser = await puppeteer.launch({
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+      executablePath: "/opt/render/.cache/puppeteer/chrome/linux-133.0.6943.126/chrome-linux64/chrome",
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });

41846bd6642a925e42e30abf12e845b24ad6d5fd
SaveMeASpark20 <legchris12@gmail.com>
2025-03-01 22:52:17 +0800

    try without headless

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ const scrape = async () => {
     console.log("Launching Puppeteer...");
 
     const browser = await puppeteer.launch({
-      headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 

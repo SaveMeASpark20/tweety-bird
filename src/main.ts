4ec7dba750b6cc3673eab6a86c97440f83be5671
SaveMeASpark20 <legchris12@gmail.com>
2025-03-01 19:28:22 +0800

    try to not use the executablepath

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 3000;
 
     const browser = await puppeteer.launch({
       headless: true,
-      executablePath: "/opt/render/.cache/puppeteer/chrome/linux-133.0.6943.126/chrome-linux64/chrome",
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 

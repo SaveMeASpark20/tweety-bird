ea0db4a68d01aefc333d6e1d5e229eee3e1e1985
SaveMeASpark20 <legchris12@gmail.com>
2025-03-01 22:32:52 +0800

    try the provided code on devto

--- render-build.sh
+++ render-build.sh
@@ -1,29 +1,25 @@
 #!/usr/bin/env bash
-# Exit on error
+#exit on error
 set -o errexit
 
-# Install dependencies
+#Install dependencies
 npm install
 
 # Uncomment this line if you need to build your project
 npm run build
-
 # Ensure the Puppeteer cache directory exists
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
-mkdir -p "$PUPPETEER_CACHE_DIR"
+mkdir -p $PUPPETEER_CACHE_DIR
 
 # Install Puppeteer and download Chrome
 npx puppeteer browsers install chrome
 
-echo "🔍 Searching for Chrome..."
-find / -name "chrome" 2>/dev/null
-
-
 # Store/pull Puppeteer cache with build cache
-if [[ -d "$PUPPETEER_CACHE_DIR" ]]; then
-    echo "🔄 Storing Puppeteer Cache in Build Cache"
-    cp -R $PUPPETEER_CACHE_DIR /opt/render/.cache/puppeteer/chrome/
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+echo "...Copying Puppeteer Cache from Build Cache"
+# Copying from the actual path where Puppeteer stores its Chrome binary
+cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
 else
-    echo "📥 Copying Puppeteer Cache from Build Cache"
-    cp -R /opt/render/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+echo "...Storing Puppeteer Cache in Build Cache"
+cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
 fi

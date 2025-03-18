eda803d75fb43e7ec40aecc7d7c129a0caf5c32b
SaveMeASpark20 <legchris12@gmail.com>
2025-03-18 21:25:24 +0800

    fix the executablepath

--- src/config.ts
+++ src/config.ts
@@ -7,10 +7,10 @@ export const config = {
             "--no-sandbox",
             "--single-process",
             "--no-zygote",
-            
+            "--disable-gpu"
         ],
-        executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-        //executablePath : process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium",
+        //executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+        executablePath : process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium",
     },
 
     xAccounts :  [
